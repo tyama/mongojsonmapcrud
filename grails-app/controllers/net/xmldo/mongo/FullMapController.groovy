964ed71acd8c5263dc9fd4f5c11a47b37d63058a
Tsuyoshi Yamamoto <mottsnite@gmail.com>
2010-12-01 18:28:02 +0900

    escaping special characters

--- grails-app/controllers/net/xmldo/mongo/FullMapController.groovy
+++ grails-app/controllers/net/xmldo/mongo/FullMapController.groovy
@@ -134,4 +134,16 @@ class FullMapController {
             redirect(action: "list")
         }
     }
+
+	private static String escapeRegex(String s) {
+		StringBuilder sb = new StringBuilder()
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (c == '\\' || c == '+' || c == '-' || c == '!' || c == '(' || c == ')' || c == ':'
+				|| c == '^' || c == '[' || c == ']' || c == '\"' || c == '{' || c == '}' || c == '~'
+				|| c == '*' || c == '?' || c == '|' || c == '&') { sb.append('\\') }
+			sb.append(c)
+		}
+		return sb.toString()
+	}
 }
